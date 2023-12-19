6f9d862dc3746d3dc8883b5a1dbe322cc43cf8e7
daniev <danievprogramming@gmail.com>
2023-12-19 18:34:30 +0100

    Change url structure to a more sustainable one

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 import flask
+import datetime
 
 # add --host=0.0.0.0 to make visible externally...
 
 app = flask.Flask(__name__)
+
+
+def generate_slug(task_title: str) -> str:
+    """Add task title and current time to create slug"""
+    timenow = str(datetime.datetime.now())
+    return task_title + "_" + timenow
+
 
 @app.route("/")
 def info():
@@ -16,24 +24,23 @@ def infoApi():
     # add template...
     return "<h1>Velkommen til Husansvars web api!</h1>"
 
-@app.route("/api/v1/mia/tasks")
-def getMiasTasks():
-    return [{
-        "id": 1,
-    },
-    {
-        "id": 2,
-    }]
+
+@app.route("/api/v1/tasks/<string:user>")
+def getMiasTasks(user):
+    return [
+        {
+            "id": 1,
+        },
+        {
+            "id": 2,
+        },
+    ]
 
 
-@app.route("/api/v1/mia/tasks/<int:id>")
-def getMiaTask(id):
+@app.route("/api/v1/tasks/<string:slug><string:user>")
+def getMiaTask(slug, user):
+    # lookup task by id or slug,
+    # check if the user is the correct user
     return {
         "id": id,
     }
-
-
-@app.errorhandler(404)
-def handleError():
-    # add template...
-    return "Wrong url, page not found";
--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,8 @@
 from dataclasses import dataclass
-
-@dataclass
-class User():
-    name: str
-
 @dataclass
 class Task():
     id: int
     title: str
-    assignee: User
+    assignee: str
 
 
