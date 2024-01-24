081b5a9fe03f6e697ac729cc3f29e5439c20315a
daniev <danievprogramming@gmail.com>
2024-01-24 19:54:07 +0100

    Fix linting and formatting

--- frontend/src/lib/api/urls.ts
+++ frontend/src/lib/api/urls.ts
@@ -1,5 +1,4 @@
 /** The base URL for the API */
-const BASE_URL = "http://localhost:5000/";
-export const API_URL = `${BASE_URL}api/v1/`
-export const TASK_URL = `${API_URL}tasks/`
-
+const BASE_URL = 'http://localhost:5000/';
+export const API_URL = `${BASE_URL}api/v1/`;
+export const TASK_URL = `${API_URL}tasks/`;
